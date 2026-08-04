452c031ec970626a231c515cef23a4c64254124c
SuGwonPark <92679565+SuGwonPark@users.noreply.github.com>
2026-08-04 13:36:01 +0900

    no message

--- server/cppProject/Main.cpp
+++ server/cppProject/Main.cpp
@@ -8,14 +8,6 @@
 int main() {
 	DBConnection db;
 
-	// 서버 시작 시 기본 방 생성
-	/*RoomManager::GetInstance().CreateRoom(1, "초보자의 마을");
-	RoomManager::GetInstance().CreateRoom(2, "전투의 광장");
-	RoomManager::GetInstance().CreateRoom(3, "보스 레이드");
-
-	RoomManager::GetInstance().PrintAll();*/
-
-
 	// Mysql 연결
 	if (!db.Connect("127.0.0.1", "root", "gpdlgh1234!@#$", "gamedb", 33060)) {
 		return -1;
